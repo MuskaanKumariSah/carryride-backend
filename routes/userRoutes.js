13999109aed453e4e53776cadcf4969889b65588
Muskaan Kumari Sah <muskaansah04@gmail.com>
2026-07-15 03:19:57 +0530

    Add OTP verification flow

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,44 +1,21 @@
 const express = require("express");
-
 const router = express.Router();
 
 const {
     registerUser,
-    loginUser,
     verifyOtp,
     resendOtp,
+    loginUser,
     getUserRideHistory,
     getUserLuggageHistory
 } = require("../controllers/userController");
 
-router.post(
-    "/register",
-    registerUser
-);
+router.post("/register", registerUser);
+router.post("/verify-otp", verifyOtp);
+router.post("/resend-otp", resendOtp);
+router.post("/login", loginUser);
 
-router.post(
-    "/login",
-    loginUser
-);
-
-router.post(
-    "/verify-otp",
-    verifyOtp
-);
-
-router.post(
-    "/resend-otp",
-    resendOtp
-);
-
-router.get(
-    "/rides/:userId",
-    getUserRideHistory
-);
-
-router.get(
-    "/luggage/:userId",
-    getUserLuggageHistory
-);
+router.get("/rides/:userId", getUserRideHistory);
+router.get("/luggage/:userId", getUserLuggageHistory);
 
 module.exports = router;
